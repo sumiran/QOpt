39dd8731f7d7ba281766f578995e9959cef12654
Sumiran R Shah <srs2222@islamabad.clic.cs.columbia.edu>
2014-02-20 19:19:31 -0500

    RC1

--- src/BingTest.java
+++ src/BingTest.java
@@ -44,9 +44,8 @@ class Term implements Comparable<Term>
 
 class StringHelper
 {
-	//public static String[] englishStopWords = { "a", "about", "above", "above", "across", "after", "afterwards", "again", "against", "all", "almost", "alone", "along", "already", "also","although","always","am","among", "amongst", "amoungst", "amount",  "an", "and", "another", "any","anyhow","anyone","anything","anyway", "anywhere", "are", "around", "as",  "at", "back","be","became", "because","become","becomes", "becoming", "been", "before", "beforehand", "behind", "being", "below", "beside", "besides", "between", "beyond", "bill", "both", "bottom","but", "by", "call", "can", "cannot", "cant", "co", "con", "could", "couldnt", "cry", "de", "describe", "detail", "do", "done", "down", "due", "during", "each", "eg", "eight", "either", "eleven","else", "elsewhere", "empty", "enough", "etc", "even", "ever", "every", "everyone", "everything", "everywhere", "except", "few", "fifteen", "fify", "fill", "find", "fire", "first", "five", "for", "former", "formerly", "forty", "found", "four", "from", "front", "full", "further", "get", "give", "go", "had", "has", "hasnt", "have", "he", "hence", "her", "here", "hereafter", "hereby", "herein", "hereupon", "hers", "herself", "him", "himself", "his", "how", "however", "hundred", "ie", "if", "in", "inc", "indeed", "interest", "into", "is", "it", "its", "itself", "keep", "last", "latter", "latterly", "least", "less", "ltd", "made", "many", "may", "me", "meanwhile", "might", "mill", "mine", "more", "moreover", "most", "mostly", "move", "much", "must", "my", "myself", "name", "namely", "neither", "never", "nevertheless", "next", "nine", "no", "nobody", "none", "noone", "nor", "not", "nothing", "now", "nowhere", "of", "off", "often", "on", "once", "one", "only", "onto", "or", "other", "others", "otherwise", "our", "ours", "ourselves", "out", "over", "own","part", "per", "perhaps", "please", "put", "rather", "re", "same", "see", "seem", "seemed", "seeming", "seems", "serious", "several", "she", "should", "show", "side", "since", "sincere", "six", "sixty", "so", "some", "somehow", "someone", "something", "sometime", "sometimes", "somewhere", "still", "such", "system", "take", "ten", "than", "that", "the", "their", "them", "themselves", "then", "thence", "there", "thereafter", "thereby", "therefore", "therein", "thereupon", "these", "they", "thickv", "thin", "third", "this", "those", "though", "three", "through", "throughout", "thru", "thus", "to", "together", "too", "top", "toward", "towards", "twelve", "twenty", "two", "un", "under", "until", "up", "upon", "us", "very", "via", "was", "we", "well", "were", "what", "whatever", "when", "whence", "whenever", "where", "whereafter", "whereas", "whereby", "wherein", "whereupon", "wherever", "whether", "which", "while", "whither", "who", "whoever", "whole", "whom", "whose", "why", "will", "with", "within", "without", "would", "yet", "you", "your", "yours", "yourself", "yourselves", "the" };
 	public static ArrayList<String> englishStopWords = new ArrayList<String>(Arrays.asList(new String[] {"a", "about", "above", "across", "after", "again", "against", "all", "almost", "alone", "along", "already", "also", "although", "always", "among", "an", "and", "another", "any", "anybody", "anyone", "anything", "anywhere", "are", "area", "areas", "around", "as", "ask", "asked", "asking", "asks", "at", "away", "b", "back", "backed", "backing", "backs", "be", "became", "because", "become", "becomes", "been", "before", "began", "behind", "being", "beings", "best", "better", "between", "big", "both", "but", "by", "c", "came", "can", "cannot", "case", "cases", "certain", "certainly", "clear", "clearly", "come", "could", "d", "did", "differ", "different", "differently", "do", "does", "done", "down", "down", "downed", "downing", "downs", "during", "e", "each", "early", "either", "end", "ended", "ending", "ends", "enough", "even", "evenly", "ever", "every", "everybody", "everyone", "everything", "everywhere", "f", "face", "faces", "fact", "facts", "far", "felt", "few", "find", "finds", "first", "for", "four", "from", "full", "fully", "further", "furthered", "furthering", "furthers", "g", "gave", "general", "generally", "get", "gets", "give", "given", "gives", "go", "going", "good", "goods", "got", "great", "greater", "greatest", "group", "grouped", "grouping", "groups", "h", "had", "has", "have", "having", "he", "her", "here", "herself", "high", "high", "high", "higher", "highest", "him", "himself", "his", "how", "however", "i", "if", "important", "in", "interest", "interested", "interesting", "interests", "into", "is", "it", "its", "itself", "j", "just", "k", "keep", "keeps", "kind", "knew", "know", "known", "knows", "l", "large", "largely", "last", "later", "latest", "least", "less", "let", "lets", "like", "likely", "long", "longer", "longest", "m", "made", "make", "making", "man", "many", "may", "me", "member", "members", "men", "might", "more", "most", "mostly", "mr", "mrs", "much", "must", "my", "myself", "n", "necessary", "need", "needed", "needing", "needs", "never", "new", "new", "newer", "newest", "next", "no", "nobody", "non", "noone", "not", "nothing", "now", "nowhere", "number", "numbers", "o", "of", "off", "often", "old", "older", "oldest", "on", "once", "one", "only", "open", "opened", "opening", "opens", "or", "order", "ordered", "ordering", "orders", "other", "others", "our", "out", "over", "p", "part", "parted", "parting", "parts", "per", "perhaps", "place", "places", "point", "pointed", "pointing", "points", "possible", "present", "presented", "presenting", "presents", "problem", "problems", "put", "puts", "q", "quite", "r", "rather", "really", "right", "right", "room", "rooms", "s", "said", "same", "saw", "say", "says", "second", "seconds", "see", "seem", "seemed", "seeming", "seems", "sees", "several", "shall", "she", "should", "show", "showed", "showing", "shows", "side", "sides", "since", "small", "smaller", "smallest", "so", "some", "somebody", "someone", "something", "somewhere", "state", "states", "still", "still", "such", "sure", "t", "take", "taken", "than", "that", "the", "their", "them", "then", "there", "therefore", "these", "they", "thing", "things", "think", "thinks", "this", "those", "though", "thought", "thoughts", "three", "through", "thus", "to", "today", "together", "too", "took", "toward", "turn", "turned", "turning", "turns", "two", "u", "under", "until", "up", "upon", "us", "use", "used", "uses", "v", "very", "w", "want", "wanted", "wanting", "wants", "was", "way", "ways", "we", "well", "wells", "went", "were", "what", "when", "where", "whether", "which", "while", "who", "whole", "whose", "why", "will", "with", "within", "without", "work", "worked", "working", "works", "would", "x", "y", "year", "years", "yet", "you", "young", "younger", "youngest", "your", "yours", "z"}));
-	public static ArrayList<String> internetStopWords = new ArrayList<String>(Arrays.asList(new String[]{ "http", "www", "index", "html", "encyclopedia", "dictionary", "en", "asp", "aspx", "php", "com", "org", "ie", "page", "utf8", "utf16", "utf32", "utf64", "etc" }));
+	public static ArrayList<String> internetStopWords = new ArrayList<String>(Arrays.asList(new String[]{ "http", "www", "index", "html", "encyclopedia", "dictionary", "definition" , "en", "free", "asp", "aspx", "php", "com", "org", "ie", "page", "utf8", "utf16", "utf32", "utf64", "etc" }));
 	
 	public static String sanitizeStringBag(String bag, String query)
 	{
@@ -101,9 +100,9 @@ class StringHelper
 		int diff = Math.max(s1Len - lcsLength, s2.length() - lcsLength );
 		int maxLen = Math.max(s1Len, s2len);
 		
-		if(diff < 1 && maxLen > 3)	return true;
+		if(diff < 1 && maxLen > 5)	return true;
 		
-		if(diff <= (maxLen*1.0)/3)	return true;
+		if(diff <= (maxLen*1.0)/5)	return true;
 		
 		return false;
 		
@@ -191,12 +190,17 @@ public class BingTest {
 		URLConnection urlConnection = url.openConnection();
 		urlConnection.setRequestProperty("Authorization", "Basic " + accountKeyEnc);
 		
-		InputStream inputStream = (InputStream) urlConnection.getContent();		
-		byte[] contentRaw = new byte[urlConnection.getContentLength()];
-		inputStream.read(contentRaw);
-		String content = new String(contentRaw);
+		BufferedReader br = new BufferedReader(new InputStreamReader(urlConnection.getInputStream(), "UTF-8"));
 		
-		document = DocumentHelper.parseText(content);
+		String str = "";
+		String line = "";
+		
+		while((line = br.readLine()) != null)
+		{
+			str += line;
+		}
+		
+		document = DocumentHelper.parseText(str);
 	}
 
 	public static void analyze() throws Exception
@@ -398,14 +402,6 @@ public class BingTest {
 					}
 					String[] nonRelevantURLs = StringHelper.sanitizeStringBag(allNonRelevantURLsString, q).split(" ");
 					
-					/*
-					Arrays.sort(relevantTitles);
-					Arrays.sort(relevantDescriptions);
-					Arrays.sort(relevantURLs);
-					Arrays.sort(nonRelevantTitles);
-					Arrays.sort(nonRelevantDescriptions);
-					Arrays.sort(nonRelevantURLs);
-					*/
 					
 					ArrayList<Term> terms = new ArrayList<Term>();
 					
@@ -452,7 +448,6 @@ public class BingTest {
 					
 					q += " " + newTerm1 + " " + newTerm2 ;
 					
-					//q = StringHelper.matchRelativeOrder(q, allRelevantDescriptionsString);
 				}
 			}
 			while(currentP < targetP && currentP > 0);
